a0172f49793a77d34b0d80eceb1e981ff7fb6113
Sizwe-Shadow <mzilasizwe@gmail.com>
2020-03-04 23:41:40 +0200

    This is the full functional app with login and register created without the database

---
+++ app/src/main/java/com/example/thewitsapp/Login.java
@@ -0,0 +1,27 @@
+package com.example.thewitsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button register_button = findViewById(R.id.Signin);
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
