6b4050146b615627d4d50955420c62f6f3eb702e
Takobz <kobelakhutso.kk@gmail.com>
2020-03-07 00:11:00 +0200

    I added the server communicator for internet purposes and I updated register and login. The menuActivity is for select the sub parts of the app. And tables are up in ssh if you guys wanna check them.

--- app/src/main/java/com/example/thewitsapp/Login.java
+++ app/src/main/java/com/example/thewitsapp/Login.java
@@ -7,18 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+import java.io.BufferedReader;
+
+public class Login extends AppCompatActivity { //Activity should actually be the main. But some did their things.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button register_button = findViewById(R.id.Signin);
+        Button register_button = findViewById(R.id.Signup);
+        Button login_button = findViewById(R.id.Login);
+
         register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/thewitsapp/ServerCommunicator.java
@@ -0,0 +1,68 @@
+package com.example.thewitsapp;
+
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+
+public abstract class ServerCommunicator extends AsyncTask<String, String, String> {
+
+    private final  String address;
+    private final ContentValues params;
+
+    protected  ServerCommunicator(String address, ContentValues params) {
+        this.address = address;
+        this.params = params;
+    }
+
+    @Override
+    protected abstract void onPreExecute();  //while loading
+
+    @Override
+    protected String doInBackground(String... strings) { //while enstablishing connection....
+
+        try {
+            URL url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setConnectTimeout(2000);
+
+            if(params.size()>0){
+                Uri.Builder builder = new Uri.Builder();
+
+                for(String s : params.keySet()) builder.appendQueryParameter(s,params.getAsString(s));
+                String query = builder.build().getEncodedQuery();
+                OutputStream os = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, Charset.forName("UTF-8")));
+                assert query != null;
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String response = br.readLine();
+            br.close();
+            return response;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+    @Override
+    protected abstract void onPostExecute(String output); //after connection what happens.....
+}
