868caf92eb75b0b44307678523827e02322d6f2c
Earlstein <earl4xolani@gmail.com>
2020-06-15 02:57:38 +0200

    Test run3.0

---
+++ app/src/androidTest/java/com/example/thewitsapp/MainActivityTest.java
@@ -0,0 +1,47 @@
+package com.example.thewitsapp;
+
+import android.Manifest;
+import android.widget.EditText;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void testEnterDetailClickBotton(){
+        onView(withId(R.id.studentNumberLogin))
+                .perform(typeText("1872817"), closeSoftKeyboard())
+                .check(matches(withHint("Student Number")));
+
+//        onView(withText(R.id.passwordLogin))
+//                .perform(typeText("khutso.1999"), closeSoftKeyboard());
+//                .check(matches(withHint("Password")));
+
+        onView(withId(R.id.login_button))
+                .perform(click());
+
+
+    }
+
+}
