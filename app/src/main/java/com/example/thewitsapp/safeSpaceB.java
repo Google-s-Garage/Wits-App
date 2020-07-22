e7f6e73c24a757a2dbe5e0d16151d69ee3b428ed
Takobz <kobelakhutso.kk@gmail.com>
2020-07-22 13:48:38 +0200

    Just commit

--- app/src/main/java/com/example/thewitsapp/safeSpaceB.java
+++ app/src/main/java/com/example/thewitsapp/safeSpaceB.java
@@ -123,7 +123,7 @@ public class safeSpaceB extends AppCompatActivity {
                             if(output.equals("1")){
 
                                 Toast.makeText(safeSpaceB.this,"Replied",Toast.LENGTH_SHORT).show();
-                                myDiscEditText.setText(" ");
+                                myDiscEditText.setText("");
 
                                 //Essentially what this does it get the messages again
                                 //With the new one now added
