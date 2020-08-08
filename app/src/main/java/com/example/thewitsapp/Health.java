500beb5a8c47753cdf9d1e60c2e8330865b6c64a
Takobz <kobelakhutso.kk@gmail.com>
2020-08-08 22:07:48 +0200

    creat

---
+++ app/src/main/java/com/example/thewitsapp/Health.java
@@ -0,0 +1,14 @@
+package com.example.thewitsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Health extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health);
+    }
+}
