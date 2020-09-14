4490bb23818c33611ca9d9680f9f93ababea81f3
Phaswamotse <1810589@students.wits.ac.za>
2020-09-14 15:12:22 +0200

    finalised the health design

---
+++ app/src/main/java/com/example/thewitsapp/Recipe_activity.java
@@ -0,0 +1,13 @@
+package com.example.thewitsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recipe_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_activity);
+    }
+}
